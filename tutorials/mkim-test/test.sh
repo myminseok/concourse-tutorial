b3f73eedf2b85ded770b9864baa98f333e14455a
MinSeok Kim <myminseok@gmail.com>
2019-04-06 22:11:08 +0900

    x

---
+++ tutorials/mkim-test/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export fly_target=${fly_target:-tutorial}
+echo "Concourse API target ${fly_target}"
+echo "Tutorial $(basename $DIR)"
+
+pushd $DIR
+  fly -t ${fly_target} set-pipeline -p test-job -c test-job.yml -n
+  fly -t ${fly_target} unpause-pipeline -p test-job
+  fly -t ${fly_target} trigger-job -w -j test-job/job-hello-world
+popd
