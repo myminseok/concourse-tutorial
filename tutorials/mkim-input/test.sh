18f326385e1ff952a6b66c457ba0e2afcea3d15e
MinSeok Kim <myminseok@gmail.com>
2019-04-08 23:01:47 +0900

    adding sample mkim

---
+++ tutorials/mkim-input/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export fly_target=${fly_target:-tutorial}
+echo "Concourse API target ${fly_target}"
+echo "Tutorial $(basename $DIR)"
+
+pushd $DIR
+  fly -t ${fly_target} set-pipeline -p test-job -c test-job.yml -v test=hi -n
+  fly -t ${fly_target} unpause-pipeline -p test-job
+  fly -t ${fly_target} trigger-job -w -j test-job/job-hello-world
+popd
--- tutorials/mkim-output/test.sh
+++ tutorials/mkim-output/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export fly_target=${fly_target:-tutorial}
+export fly_target=${fly_target:-demo}
 echo "Concourse API target ${fly_target}"
 echo "Tutorial $(basename $DIR)"
 
 pushd $DIR
-  fly -t ${fly_target} set-pipeline -p test-job -c test-job.yml -n
+  fly -t ${fly_target} set-pipeline -p test-job -c test-job.yml -v test=hi -n
   fly -t ${fly_target} unpause-pipeline -p test-job
-  fly -t ${fly_target} trigger-job -w -j test-job/job-hello-world
+  fly -t ${fly_target} trigger-job -w -j test-job/job-hello-world -w
 popd
